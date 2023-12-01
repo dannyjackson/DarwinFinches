2fe9b4d5e8d3d1276069d80ec10a8598dd7400e1
Danny Jackson <dnjacks4@asu.edu>
2023-12-01 10:56:09 -0700

    Update fstscans.r

--- fstscans.r
+++ fstscans.r
@@ -6,34 +6,30 @@ args = commandArgs()
 outDir <- args[6]
 name <- args[7]
 
-
-pi.all <- read.table(paste0(outDir,"/",name,".chroms.windowed.pi"),header=T)
-pi.subset<-pi.all[complete.cases(pi.all),]
-
-SNP<-c(1: (nrow(pi.subset)))
-
-lower = min(pi.subset$PI)
-upper = max(pi.subset$PI)
-cutoff = upper - ((upper-lower)*0.05)
-
-LessThanCutoff <- pi.subset$PI < cutoff
-
-myBg <- !LessThanCutoff
-
-
-
-mydf<-data.frame(SNP,myBg,pi.subset)
-
-
-
-
-R
 library(qqman)
+
 fst<-read.table(paste0(outDir,"/",name,"chroms.weir.formanhattan.fst"), header=TRUE)
+
 fstsubset<-fst[complete.cases(fst),]
+
+
+xu <- mean(fstsubset$WEIGHTED_FST)
+s <- sd(fstsubset$WEIGHTED_FST)
+fstsubset$ZFST = (fstsubset$WEIGHTED_FST - xu)/s
+
 SNP<-c(1: (nrow(fstsubset)))
+
 mydf<-data.frame(SNP,fstsubset)
 
-pdf(file = paste0(outDir,"/",name,".chroms.fst.pdf", width = 20, height = 7, useDingbats=FALSE)
+
+write.csv(mydf[ which(mydf$ZFST>='5'),], paste0(outDir,"/",name,".zfst_sig.csv"), row.names=FALSE)
+
+
+pdf(file = paste0(outDir,"/",name,".chroms.fst.pdf"), width = 20, height = 7, useDingbats=FALSE)
 print(manhattan(mydf,chr="CHROM",bp="SNP",p="WEIR_AND_COCKERHAM_FST",snp="SNP",logp=FALSE,ylab="Weighted Weir and Cockerham Fst"))
+dev.off()
+
+
+pdf(file = paste0(outDir,"/",name,".chroms.zfst.pdf"), width = 20, height = 7, useDingbats=FALSE)
+print(manhattan(mydf,chr="CHROM",bp="BIN_START",p="ZFST",snp="SNP",logp=FALSE,ylab="Weighted Weir and Cockerham Fst", cex = 0.2))
 dev.off()
