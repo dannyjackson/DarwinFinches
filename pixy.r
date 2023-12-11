a5f75a587db0ade91ea23bbe3da5df19b0584425
Danny Jackson <dnjacks4@asu.edu>
2023-12-11 16:05:18 -0700

    Update pixy.r

--- pixy.r
+++ pixy.r
@@ -91,7 +91,7 @@ write.csv(mydf[ which(mydf$myBg=='TRUE'),], paste0(outDir,"/fst/",name,".fst_sig
 
 ## dxy 
 
-dxy.all <- read.table(paste0(outDir,"/cra.chroms.pixy_dxy.txt"),header=T)
+dxy.all <- read.table(paste0(outDir,"/dxy/cra.chroms.pixy_dxy.txt"),header=T)
 dxy.subset<-dxy.all[complete.cases(dxy.all),]
 
 SNP<-c(1: (nrow(dxy.subset)))
@@ -108,11 +108,11 @@ myBg <- !LessThanCutoff
 
 mydf<-data.frame(SNP,myBg,dxy.subset)
 
-pdf(file = paste0(outDir,"/",name,".dxy_hist.pdf"), width = 10, height = 5, useDingbats=FALSE)
+pdf(file = paste0(outDir,"/dxy/",name,".dxy_hist.pdf"), width = 10, height = 5, useDingbats=FALSE)
 hist(dxy.subset$avg_dxy,br=50)
 dev.off()
 
-pdf(file = paste0(outDir,"/",name,".dxy.pdf"), width = 20, height = 7, useDingbats=FALSE)
+pdf(file = paste0(outDir,"/dxy/",name,".dxy.pdf"), width = 20, height = 7, useDingbats=FALSE)
 
 plot(avg_dxy ~ SNP, col= "white", pch = 21, bg=ifelse(myBg  == "TRUE", 'red', 'gray'),
      data = mydf,
@@ -126,7 +126,7 @@ dev.off()
 
 # this gives regions of significance 
 
-write.csv(mydf[ which(mydf$myBg=='TRUE'),], paste0(outDir,"/",name,".dxy_sig.csv"), row.names=FALSE)
+write.csv(mydf[ which(mydf$myBg=='TRUE'),], paste0(outDir,"/dxy/",name,".dxy_sig.csv"), row.names=FALSE)
 
 
 
